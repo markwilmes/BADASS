3a1b2b66aad0d375c3d3968a47c16757ec0faef8
mark-wilmes <mawi0949@colorado.edu>
2017-10-11 20:50:11 -0600

    Updated gradle app build to include correct external libraries

--- app/src/main/java/com/teambadass/csci3308/badass/MapsActivity.java
+++ app/src/main/java/com/teambadass/csci3308/badass/MapsActivity.java
@@ -1,6 +1,5 @@
 package com.teambadass.csci3308.badass;
 
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -11,36 +10,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static com.teambadass.csci3308.badass.R.id.location;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationCallback mLocationCallback;
+    private com.google.android.gms. mFusedLocationClient;
+    private com.google.android.gms.maps.LocationCallback mLocationCallback;
 
+    private Boolean mRequestingLocationUpdates;
+    private com.google.android.gms.location.LocationRequest mLocationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        updateValuesFromBundle(savedInstanceState);
-
-        mLocationCallback = new LocationCallback() {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            mFusedLocationClient = com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(this);
+            updateValuesFromBundle(savedInstanceState);
+            mLocationCallback = new com.google.android.gms.location.LocationCallback() {
             @Override
-            public void onLocationResult(LocationResult locationResult) {
-                for (Location location : locationResult.getLocations()) {
+            public void onLocationResult(com.google.android.gms.location.LocationResult locationResult) {
+                    for (android.location.Location location : locationResult.getLocations()) {
                     // Update UI with location data
                     // ...
-                }
-            };
+                    }
+            }
         };
-
     }
     protected void onResume() {
         super.onResume();
@@ -48,58 +44,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             startLocationUpdates();
         }
     }
-
     private void startLocationUpdates() {
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-                mLocationCallback,
-                null /* Looper */);
+
+    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+            mLocationCallback,
+            null /* Looper */);
     }
-
-    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-        @Override
-        public void onSuccess(Location location) {
-            // Got last known location. In some rare situations this can be null.
-            if (location != null) {
-                // Logic to handle location object
-            }
+    protected void createLocationRequest() {
+            com.google.android.gms.location.LocationRequest mLocationRequest = new com.google.android.gms.location.LocationRequest();
+            mLocationRequest.setInterval(10000);
+            mLocationRequest.setFastestInterval(5000);
+            mLocationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    com.google.android.gms.location.LocationSettingsRequest.Builder builder = new com.google.android.gms.location.LocationSettingsRequest.Builder()
+    .addLocationRequest(mLocationRequest);
+    com.google.android.gms.location.LocationSettingsRequest.Builder builder = new com.google.android.gms.location.LocationSettingsRequest.Builder();
+    // ...
+    com.google.android.gms.location.SettingsClient client = com.google.android.gms.location.LocationServices.getSettingsClient(this);
+    com.google.android.gms.tasks.Task<com.google.android.gms.location.LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+    mFusedLocationClient.getLastLocation()
+    .addOnSuccessListener(this, new com.google.android.gms.tasks.OnSuccessListener<android.location.Location>() {
+    public void onSuccess(android.location.Location) {
+        // Got last known location. In some rare situations this can be null.
+        if (android.location.Location != null) {
+            // Logic to handle location object
         }
-    });
+    }});
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    /* we just add a marker near Sydney, Australia.
+    * If Google Play services is not installed on the device, the user will be prompted to install
+    * it inside the SupportMapFragment. This method will only be triggered once the user has
+    * installed Google Play services and returned to the app.
+    */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
     protected void onSaveInstanceState(Bundle outState) {
         outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
-                mRequestingLocationUpdates);
+        mRequestingLocationUpdates);
         // ...
         super.onSaveInstanceState(outState);
     }
-
     private void updateValuesFromBundle(Bundle savedInstanceState) {
         // Update the value of mRequestingLocationUpdates from the Bundle.
         if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
             mRequestingLocationUpdates = savedInstanceState.getBoolean(
-                    REQUESTING_LOCATION_UPDATES_KEY);
+            REQUESTING_LOCATION_UPDATES_KEY);
         }
-
         // ...
-
         // Update UI to match restored state
         updateUI();
     }
