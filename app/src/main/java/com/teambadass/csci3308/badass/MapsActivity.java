dbd93fc6271bfe4ddf42ee63446b07dabdc76cff
mark-wilmes <mawi0949@colorado.edu>
2017-11-17 07:22:18 -0700

    Fixing bugs

--- app/src/main/java/com/teambadass/csci3308/badass/MapsActivity.java
+++ app/src/main/java/com/teambadass/csci3308/badass/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.teambadass.csci3308.badass;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,42 +19,55 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
-    private com.google.android.gms. mFusedLocationClient;
-    private com.google.android.gms.maps.LocationCallback mLocationCallback;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
 
     private Boolean mRequestingLocationUpdates;
     private com.google.android.gms.location.LocationRequest mLocationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_maps);
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-            .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-            mFusedLocationClient = com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(this);
-            updateValuesFromBundle(savedInstanceState);
-            mLocationCallback = new com.google.android.gms.location.LocationCallback() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mFusedLocationClient = com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(this);
+        updateValuesFromBundle(savedInstanceState);
+        mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(com.google.android.gms.location.LocationResult locationResult) {
-                    for (android.location.Location location : locationResult.getLocations()) {
+                for (android.location.Location location : locationResult.getLocations()) {
                     // Update UI with location data
                     // ...
-                    }
+                }
             }
         };
     }
+
     protected void onResume() {
         super.onResume();
         if (mRequestingLocationUpdates) {
             startLocationUpdates();
         }
     }
+
     private void startLocationUpdates() {
 
-    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-            mLocationCallback,
-            null /* Looper */);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
     }
     protected void createLocationRequest() {
             com.google.android.gms.location.LocationRequest mLocationRequest = new com.google.android.gms.location.LocationRequest();
@@ -56,20 +75,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mLocationRequest.setFastestInterval(5000);
             mLocationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
-    com.google.android.gms.location.LocationSettingsRequest.Builder builder = new com.google.android.gms.location.LocationSettingsRequest.Builder()
+    com.google.android.gms.location.LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
     .addLocationRequest(mLocationRequest);
-    com.google.android.gms.location.LocationSettingsRequest.Builder builder = new com.google.android.gms.location.LocationSettingsRequest.Builder();
+    //com.google.android.gms.location.LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
     // ...
     com.google.android.gms.location.SettingsClient client = com.google.android.gms.location.LocationServices.getSettingsClient(this);
     com.google.android.gms.tasks.Task<com.google.android.gms.location.LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
     mFusedLocationClient.getLastLocation()
     .addOnSuccessListener(this, new com.google.android.gms.tasks.OnSuccessListener<android.location.Location>() {
-    public void onSuccess(android.location.Location) {
+        public void onSuccess (android.location.Location) {
         // Got last known location. In some rare situations this can be null.
-        if (android.location.Location != null) {
+        if (null != android.location.Location) {
             // Logic to handle location object
         }
-    }});
+        }});
 
     /* we just add a marker near Sydney, Australia.
     * If Google Play services is not installed on the device, the user will be prompted to install
