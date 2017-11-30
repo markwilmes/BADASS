071b78b8bc765b65b94c40e8205287245ea103f9
mark-wilmes <mawi0949@colorado.edu>
2017-11-30 16:37:58 -0700

    Moved login activity code to main, changed code to work with current setup of server. Ran into Error 400 malformed request issue.

--- app/src/main/java/com/teambadass/csci3308/badass/MainActivity.java
+++ app/src/main/java/com/teambadass/csci3308/badass/MainActivity.java
@@ -1,18 +1,101 @@
 package com.teambadass.csci3308.badass;
 
-import android.content.Context;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        TextView text = (TextView) findViewById(R.id.display);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
+        final EditText user = (EditText) findViewById(R.id.editText);
+        final EditText pass = (EditText) findViewById(R.id.editText3);
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String userString = user.getText().toString();
+                final String passString = pass.getText().toString();
+                Thread loginThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        login(userString,passString);
+                    }
+                });
+                loginThread.start();
+            }
+        });
+    }
+
+    protected int login(String username, String password) {
+        Intent intent = new Intent(this, MainActivity.class);
+        try {
+
+
+            URL url;
+            String response = "";
+
+            try {
+                url = new URL("http://10.0.0.187/queryDB");
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept","application/json");
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                JSONObject postParams = new JSONObject();
+                postParams.put("username", username);
+                postParams.put("password", password);
+
+                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                os.writeBytes(postParams.toString());
+
+                os.flush();
+                os.close();
+
+
+                int responseCode = conn.getResponseCode();
+                System.out.println(responseCode);
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                        System.out.println(response);
+                    }
+                    return responseCode;
+                } else {
+                    response = "";
+                    return responseCode;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 404;
     }
 }
