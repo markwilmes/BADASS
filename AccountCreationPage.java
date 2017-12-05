cc1e23e8a85ccee01028a3580fff8a0462e7d2fc
CompBioJasmine <jasmine.bascom@gmail.com>
2017-12-05 15:45:36 -0700

    expanding some of the documentation

---
+++ AccountCreationPage.java
@@ -0,0 +1,78 @@
+/*
+* Defines AccountCreationPage class
+* Like Login, has attributes for username, password, button, and a clicklistener
+*
+*/
+
+package com.teambadass.csci3308.badass;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/*
+ * Created by markw on 11/3/2017.
+ */
+
+public class AccountCreationPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this,MainActivity.class);
+        setContentView(R.layout.activity_create_account);
+        final EditText user = (EditText)findViewById(R.id.email); //username
+        final EditText pass = (EditText)findViewById(R.id.password); //password
+        Button button = (Button)findViewById(R.id.email_sign_in_button); //submit button
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String userString = user.getText().toString(); //store username
+                final String passString = pass.getText().toString(); //store password
+                Thread accountThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //submitAccount(userString,passString);
+                        startActivity(intent);
+                    }
+                });
+                accountThread.start();
+
+            }
+        });
+    }
+
+    public void submitAccount(String username, String password){
+        try {
+            URL url = new URL("http://10.0.0.187/insertData");
+
+            JSONObject params = new JSONObject(); //JSON object sends username and password
+            params.put("username",username);
+            params.put("password",password);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+            writer.write(params.toString());
+
+            //writer.flush();
+            writer.close();
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,215 @@
+/*
+*  Defines MainActivity class
+*  Instantiates XML pages and requests permissions
+*
+*/
+
+package com.teambadass.csci3308.badass;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.teambadass.csci3308.badass.BluetoothActivityFinal.ChatMain;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    String provider;
+    @Override
+    //instantiate login
+    protected void onCreate(Bundle savedInstanceState) {
+        final Intent accountCreation = new Intent(this, AccountCreationPage.class);
+        super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        setContentView(R.layout.activity_login_page);
+        final EditText user = (EditText) findViewById(R.id.editText);
+        final EditText pass = (EditText) findViewById(R.id.editText3);
+        final Button button = findViewById(R.id.button1);
+        final Button button1 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String userString = user.getText().toString();
+                final String passString = pass.getText().toString();
+                Thread loginThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        login(userString, passString);
+                    }
+                });
+                loginThread.start();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(accountCreation);
+            }
+        });
+    }
+
+    protected void login(String username, String password) {
+        try {
+
+
+            URL url;
+            String response = "";
+
+            try {
+                url = new URL("http://192.168.30.204/queryDB");
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                conn.setRequestMethod("POST");
+
+                JSONObject postParams = new JSONObject();
+                postParams.put("username", username);
+                postParams.put("password", password);
+
+                System.out.println(postParams);
+
+                OutputStreamWriter os = new OutputStreamWriter(conn.getOutputStream());
+                os.write(postParams.toString());
+
+                //os.flush();
+                os.close();
+
+
+                int responseCode = conn.getResponseCode();
+                System.out.println(responseCode);
+                if (responseCode == 200) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                    if (response != null) {
+                        Intent bluetooth = new Intent(this, ChatMain.class);
+                        startActivity(bluetooth);
+                    }
+                } else {
+                    response = "";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    //Check location permissions
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    // Check permission results
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (checkLocationPermission()) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                //Request location updates:
+
+            }
+        }
+
+    }
+}
